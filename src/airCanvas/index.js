9e5acc5758fce738ebf427e5560510274f5442b6
Harinath Reddy <harinathreddy.t@skil.ai>
2023-01-29 12:37:12 +0530

    ui integration

--- src/airCanvas/index.js
+++ src/airCanvas/index.js
@@ -1,28 +1,61 @@
 import { Box } from "@mui/material";
 import { useState, useEffect } from "react"
 import ApiService from "../app.services";
+import "./index.css"
 
-export default function AirCanvas(){
+export default function AirCanvas() {
     const [canvas, setCanvas] = useState();
 
-    useEffect(() =>{
+    // useEffect(() => {
+    //     ApiService.airCanvas().then(
+    //         response => {
+    //             console.log(response.data.path, "path");
+    //             setCanvas(response.data.path);
+    //         },
+    //         err => {
+    //             console.log(err.response);
+    //         }
+    //     )
+    // }, [])
+
+    const stream = () => {
         ApiService.airCanvas().then(
-            response =>{
-                console.log(response.data.path);
+            response => {
+                console.log(response.data.path, "path");
                 setCanvas(response.data.path);
             },
-            err =>{
+            err => {
                 console.log(err.response);
             }
         )
-      },[])
+    }
 
-    return(
-        <Box id="contentframe" 
-          sx={{backgroundColor: "#000", 
-          py: 10,
-          }}>
-            <iframe title="canvas" src={canvas} height="500" width="100%" frameBorder="0"/>     
-        </Box>
+    return (
+        <div>
+
+            <h1 id="demo">Live streaming</h1>
+
+            <div>
+                <img src={canvas} class='white-board' />
+                {/* <div class="white-board" ></div> */}
+            </div>
+            <div id="the_canvas_element_id" class="bottom-container" >
+                <button class="screenshot-btn stream-btn" onClick={stream}>Stream </button>
+
+                <button class="screenshot-btn" >Take Screenshot</button>
+                <button class="clear-btn">Clear</button>
+
+
+            </div>
+        </div>
     );
 }
+
+// <Box id="contentframe" >
+        //     <h1>Live streaming</h1>
+        //     <h1>fdhgfhwqewqq</h1>
+        //     {/* <iframe title="canvas" src={canvas} height="500" width="100%" frameBorder="0"/>   */}
+        //     <img src={canvas} width="60%" />
+
+
+        // </Box>
